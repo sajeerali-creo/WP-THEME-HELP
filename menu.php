89b927fa65b52d9219d571b46f0ca3fbfe19b3d2
Sajeerali arraka <sarraka@bloovo.com>
2024-10-22 18:41:52 +0400

    update

---
+++ menu.php
@@ -0,0 +1,7 @@
+<?php
+                        wp_nav_menu( array( 
+                            'theme_location' => 'main-menu',
+                            'menu' => 'Primary',
+                            'menu_class' => 'nav-menu, d-flex',
+                        ) );
+                        ?>
